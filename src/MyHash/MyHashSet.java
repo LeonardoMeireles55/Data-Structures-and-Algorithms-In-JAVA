325fe8c16effa9072397f53bfd7d926373db1e09
Leonardo Meireles <leomeireles55@hotmail.com>
2024-06-05 15:12:17 -0300

    chore: Refactor MyHashSet class and optimize inner join functionality

--- src/MyHash/MyHashSet.java
+++ src/MyHash/MyHashSet.java
@@ -9,7 +9,14 @@ public class MyHashSet extends MyHashTable {
         super(size);
     }
 
-    public static MyHashSet zeroHashSet = new MyHashSet(10);
+    boolean isFirstZero(int zero, MyHashSet zeroHashSet) {
+        if(zeroHashSet.size == 0) {
+            zeroHashSet.insert(zero);
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     public void insert(int value) {
@@ -36,37 +43,9 @@ public class MyHashSet extends MyHashTable {
         }
     }
 
-    public void myInnerJoin(int[] table1, int[] table2) {
-        int[] helper = new int[table1.length + table2.length];
-        int auxCounter = 0;
-        int[] result;
-        int resultCounter = 0;
+        public void myInnerJoinEnhanced(int[] table1, int[] table2) {
+            MyHashSet myZeroHashSet = new MyHashSet(5);
 
-        for (int i = 0; i < table1.length; i++) {
-            for (int j = 0; j < table2.length; j++) {
-                if (table1[i] == table2[j]) {
-                    helper[i] = table1[i];
-                }
-            }
-        
-        }
-        for (int k = 0; k < helper.length; k++) {
-            if (helper[k] != 0) {
-                auxCounter++;
-            }
-        }
-            result = new int[auxCounter];
-            for (int k = 0; k < helper.length; k++) {
-                if (helper[k] != 0) {
-                    result[resultCounter] = helper[k];
-                    resultCounter++;
-                    System.out.println("Adding " + helper[k] + " to result");
-                }
-            }
-            System.out.println(Arrays.toString(result));
-        }
-
-        public void myInnerJoinEnchated(int[] table1, int[] table2) {
             Integer[] helper = new Integer[table1.length + table2.length];    
             for (int i = 0; i < table1.length; i++) {
                     for (int j = 0; j < table2.length; j++) {
@@ -77,77 +56,78 @@ public class MyHashSet extends MyHashTable {
                 } 
 
             Integer[] resultStream = Arrays.stream(helper)
-            .filter(x -> x != null && (x != 0 || isFirstZero(x)))
+            .filter(x -> x != null && (x != 0 || isFirstZero(x, myZeroHashSet)))
             .toArray(Integer[]::new);
-
+            System.out.println("Inner Join: ");
             System.out.println(Arrays.toString(resultStream));
         }
 
-          boolean isFirstZero(int zero) {
-            if(zeroHashSet.size == 0) {
-                zeroHashSet.insert(zero);
-                return true;
-            }
-            return false;
+        public void myLeftJoinEnhanced(int[] table1, int[] table2) {
+            MyHashSet myZeroHashSet = new MyHashSet(5);
+
+            Integer[] helper = new Integer[table1.length + table2.length];    
+            for (int i = 0; i < table1.length; i++) {
+                    for (int j = 0; j < table2.length; j++) {
+                        if (table1[i] == table2[j] || table1[i] != table2[j] ) {
+                            helper[i] = table1[i];
+                        }
+                    }
+                } 
+
+            Integer[] resultStream = Arrays.stream(helper)
+            .filter(x -> x != null && (x != 0 || isFirstZero(x, myZeroHashSet)))
+            .toArray(Integer[]::new);
+            System.out.println("Left Join: ");
+            System.out.println(Arrays.toString(resultStream));
         }
 
 
-        public void myLeftJoin(int[] table1, int[] table2) {
-            int[] helper = new int[table1.length + table2.length];
-            int auxCounter = 0;
-            int[] result;
-            int resultCounter = 0;
-    
-            for (int i = 0; i < table1.length; i++) {
-                for (int j = 0; j < table2.length; j++) {
-                    if (table1[i] == table2[j] || table1[i] != table2[j] ) {
-                        helper[i] = table1[i];
+        public void myRigthJoinEnhanced(int[] table1, int[] table2) {
+            Integer[] helper = new Integer[table1.length + table2.length];    
+            MyHashSet myZeroHashSet = new MyHashSet(5);
+
+
+            for (int i = 0; i < table2.length; i++) {
+                    for (int j = 0; j < table1.length; j++) {
+                        if (table2[i] == table1[j] || table2[i] != table1[j] ) {
+                            helper[i] = table2[i];
+                        }
                     }
-                }
-            
-            }
-            for (int k = 0; k < helper.length; k++) {
-                if (helper[k] != 0) {
-                    auxCounter++;
-                }
-            }
-                result = new int[auxCounter];
-                for (int k = 0; k < helper.length; k++) {
-                    if (helper[k] != 0) {
-                        result[resultCounter] = helper[k];
-                        resultCounter++;
-                    }
-                }
-                System.out.println(Arrays.toString(result));
-            }
+                } 
+
+            Integer[] resultStream = Arrays.stream(helper)
+            .filter(x -> x != null && (x != 0 || isFirstZero(x, myZeroHashSet)))
+            .toArray(Integer[]::new);
+            System.out.println("Right Join: ");
+            System.out.println(Arrays.toString(resultStream));
+        }
 
     public static void main(String[] args) {
-        MyHashSet myHashSet = new MyHashSet(20);
+        MyHashSet myHashSet = new MyHashSet(5);
         myHashSet.insert(0);
         myHashSet.insert(1);
         myHashSet.insert(2);
         myHashSet.insert(3);
         myHashSet.insert(4);
         myHashSet.insert(5);
-        myHashSet.insert(6);
-        myHashSet.insert(7);
-        myHashSet.insert(8);
-        myHashSet.insert(9);
-        myHashSet.insert(10);
+
         // myHashSet.sortedPrint();
 
         MyHashSet myHashSet2 = new MyHashSet(5);
+        myHashSet2.insert(0);
         myHashSet2.insert(1);
         myHashSet2.insert(2);
         myHashSet2.insert(3);
         myHashSet2.insert(8);
         myHashSet2.insert(11);
-        myHashSet2.insert(0);
 
-        // myHashSet.myInnerJoin(myHashSet.table, myHashSet2.table);
-        // myHashSet.myLeftJoin(myHashSet.table, myHashSet2.table);
+        myHashSet.myInnerJoinEnhanced
+        (myHashSet.table, myHashSet2.table);
 
-        myHashSet.myInnerJoinEnchated
+        myHashSet.myRigthJoinEnhanced
+        (myHashSet.table, myHashSet2.table);
+
+        myHashSet.myLeftJoinEnhanced
         (myHashSet.table, myHashSet2.table);
     }
     
