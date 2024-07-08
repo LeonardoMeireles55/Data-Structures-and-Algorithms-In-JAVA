dbb08d6f57d658c187b1d81df5c52e18c7c9484e
leonardomeireles55 <leomeireles55@hotmail.com>
2024-07-08 09:22:49 -0300

    Refactor HashMap

--- src/MyHash/MyHashMap.java
+++ src/MyHash/MyHashMap.java
@@ -1,80 +1,81 @@
 package MyHash;
 
-public class MyHashMap {
-    private class Pair {
-        int key;
-        int value;
+public class MyHashMap<K, V> {
+    private class Pair<K, V> {
+        K key;
+        V value;
 
-        Pair(int key, int value) {
+        Pair(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
 
     private static final double LOAD_FACTOR = 0.75;
+    private int capacity;
     private int size;
-    private int used;
-    private Pair[] map;
+    private Pair<K, V> [] map;
 
-    public MyHashMap(int size) {
-        this.size = size;
-        this.used = 0;
-        this.map = new Pair[size];
+    public MyHashMap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.map = new Pair[capacity];
     }
 
-    private int hash(int key) {
-        return Math.abs(key % size);
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % capacity);
     }
 
     private void rehash() {
-        int newSize = size * 2;
+        int newSize = capacity * 2;
         Pair[] oldMap = map;
         map = new Pair[newSize];
-        size = newSize;
-        used = 0;
+        capacity = newSize;
+        size = 0;
 
-        for (Pair pair : oldMap) {
+        for (Pair<K, V> pair : oldMap) {
             if (pair != null) {
                 put(pair.key, pair.value);
             }
         }
     }
 
-    public void put(int key, int value) {
-        if ((double) used / size >= LOAD_FACTOR) {
+    public void put(K key, V value) {
+        if ((double) size / capacity >= LOAD_FACTOR) {
             rehash();
         }
 
         int index = hash(key);
         while (map[index] != null && map[index].key != key) {
-            index = (index + 1) % size;
+            index = (index + 1) % capacity;
         }
         if (map[index] == null) {
             map[index] = new Pair(key, value);
-            used++;
+            size++;
         } else {
             map[index].value = value;
         }
     }
 
-    public int get(int key) {
+    public V get(K key) {
         int index = hash(key);
         while (map[index] != null) {
-            if (map[index].key == key) {
+            if (map[index].key.equals(key)) {
+                System.out.println("Key: " + key + " Value: " + map[index].value);
                 return map[index].value;
             }
-            index = (index + 1) % size;
+            index = (index + 1) % capacity;
         }
-        return -1;
+        return null;
     }
 
-    public boolean contains(int key) {
+    public boolean contains(K key) {
         int index = hash(key);
         while (map[index] != null) {
             if (map[index].key == key) {
                 return true;
             }
-            index = (index + 1) % size;
+            index = (index + 1) % capacity;
         }
         return false;
     }
--- src/MyLeetCode/TwoSum.java
+++ src/MyLeetCode/TwoSum.java
@@ -1,70 +1,17 @@
 package MyLeetCode;
 
+import MyHash.MyHashMap;
+
 public class TwoSum {
 
-    public int[] table;
-
-    private class MyHashMap {
-        private class Pair {
-            int key;
-            int value;
-
-            Pair(int key, int value) {
-                this.key = key;
-                this.value = value;
-            }
-        }
-
-        private int size;
-        private Pair[] map;
-
-        public MyHashMap(int size) {
-            this.size = size;
-            this.map = new Pair[size];
-        }
-
-        public int hash(int key) {
-            return key % size;
-        }
-
-        public void put(int key, int value) {
-            int index = hash(key);
-            while (map[index] != null && map[index].key != key) {
-                index = (index + 1) % size;
-            }
-            map[index] = new Pair(key, value);
-        }
-
-        public int get(int key) {
-            int index = hash(key);
-            while (map[index] != null) {
-                if (map[index].key == key) {
-                    return map[index].value;
-                }
-                index = (index + 1) % size;
-            }
-            return -1;
-        }
-
-        public boolean contains(int key) {
-            int index = hash(key);
-            while (map[index] != null) {
-                if (map[index].key == key) {
-                    return true;
-                }
-                index = (index + 1) % size;
-            }
-            return false;
-        }
-    }
-
-    public int[] twoSum(int[] nums, int target) {
-        MyHashMap map = new MyHashMap(25);
+    public int[] result(int[] nums, int target) {
+        MyHashMap<Integer, Integer> map = new MyHashMap<>(100);
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
             if (map.contains(complement)) {
+                System.out.println("Complement: " + complement + " nums " + nums[i]);
                 return new int[] {map.get(complement), i};
             }
             map.put(nums[i], i);
@@ -75,10 +22,10 @@ public class TwoSum {
 
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 15, 11, 7};
         int target = 9;
 
-        int[] result = twoSum.twoSum(nums, target);
+        int[] result = twoSum.result(nums, target);
 
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
