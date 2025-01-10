fd737c539d1d71ff5ec3a3a41d2db60f93cc18bc
Leonardo Meireles <leomeireles55@hotmail.com>
2025-01-10 20:43:13 -0300

    refactor: improve code formatting and add debug statements in MySelectionSort and MyStack

--- src/MySort/MySelectionSort.java
+++ src/MySort/MySelectionSort.java
@@ -3,29 +3,31 @@ package MySort;
 import java.util.Arrays;
 
 public class MySelectionSort {
-    public  int[] myList;
+	public int[] myList;
 
-    public void mySelectionSort() {
-        for (int i = 0; i < myList.length; i++) {
-            for (int j = i + 1; j < myList.length; j++) {
-                if (myList[i] > myList[j]) {
-                    int temp = myList[i];
-                    myList[i] = myList[j];
-                    myList[j] = temp;
-//                    System.out.println(this.myList[i] + " swapping... " + this.myList[j]);
-                }
-            }
-        }
-    }
+	public void mySelectionSort() {
+		for (int i = 0; i < myList.length; i++) {
+			for (int j = i + 1; j < myList.length; j++) {
+				System.out.println("CALL COUNTER" + " --> " + (i) + j);
+				if (myList[i] > myList[j]) {
+					System.out.println(this.myList[i] + " --> swapping --> " + this.myList[j]);
+					int temp = myList[i];
+					myList[i] = myList[j];
+					myList[j] = temp;
+					System.out.println(this.myList[i] + " --> swapping --> " + this.myList[j]);
+				}
+			}
+		}
+	}
 
-    public static void main(String[] args) {
-        MySelectionSort mySelectionSort = new MySelectionSort();
-        System.out.println("Selection Sort");
-        mySelectionSort.myList = new int[]{5, 3, 4, 1, 2};
-        System.out.println("Before Sort");
-        System.out.println(Arrays.toString(mySelectionSort.myList));
-        mySelectionSort.mySelectionSort();
-        System.out.println("After Sort");
-        System.out.println(Arrays.toString(mySelectionSort.myList));
-    }
+	public static void main(String[] args) {
+		MySelectionSort mySelectionSort = new MySelectionSort();
+		System.out.println("Selection Sort");
+		mySelectionSort.myList = new int[] {5, 3, 4, 1, 2};
+		System.out.println("Before Sort");
+		System.out.println(Arrays.toString(mySelectionSort.myList));
+		mySelectionSort.mySelectionSort();
+		System.out.println("After Sort");
+		System.out.println(Arrays.toString(mySelectionSort.myList));
+	}
 }
--- src/MyStack/MyStack.java
+++ src/MyStack/MyStack.java
@@ -2,76 +2,70 @@ package MyStack;
 
 public class MyStack<T> {
 
-    private T[] elements;
-    private int stackSize;
+	private T[] elements;
+	private int stackSize;
 
-    public T[] getElements() {
-        return elements;
-    }
+	public T[] getElements() {
+		return elements;
+	}
 
-    public MyStack() {
-        elements = (T[]) new Object[2];
-        stackSize = 0;
-    }
+	public MyStack() {
+		elements = (T[]) new Object[2];
+		stackSize = 0;
+	}
 
-    //[2] -> lista com 1 elemento;
-    //[2]  -> lista com 2 elementos;
-    //[4] -> lista com 3 elementos;
-    //[4] -> lista com 4 elementos;
-    //[8] -> lista com 5 elementos;
-    private void resize(int max) {
-        T[] auxStack;
-        auxStack = (T[]) new Object[max];
-        for (int i = 0; i < stackSize; i++) {
-            auxStack[i] = elements[i];
-        }
-        elements = auxStack;
-    }
+	private void resize(int max) {
+		T[] auxStack;
+		auxStack = (T[]) new Object[max];
+		for (int i = 0; i < stackSize; i++) {
+			auxStack[i] = elements[i];
+		}
+		elements = auxStack;
+	}
 
-    public boolean isEmpty() {
-        return stackSize == 0;
-    }
+	public boolean isEmpty() {
+		return stackSize == 0;
+	}
 
-    public T peek() {
-        if(isEmpty()) {
-            throw new ArrayIndexOutOfBoundsException();
+	public T peek() {
+		if (isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException();
 
-        }
-        return elements[stackSize - 1];
-    }
+		}
+		return elements[stackSize - 1];
+	}
 
-    public void push(T t) {
-        if (stackSize == elements.length) {
-            resize(2 * elements.length);
-        }
-        elements[stackSize++] = t;
-    }
+	public void push(T t) {
+		if (stackSize == elements.length) {
+			resize(2 * elements.length);
+		}
+		elements[stackSize++] = t;
+	}
 
-    public T pop() {
-        if(isEmpty()) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        T t = elements[--stackSize];
-        if (stackSize > 0 && stackSize == elements.length / 4) {
-            resize(elements.length / 2);
-        }
-        return t;
-    }
+	public T pop() {
+		if (isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		T t = elements[--stackSize];
+		if (stackSize > 0 && stackSize == elements.length / 4) {
+			resize(elements.length / 2);
+		}
+		return t;
+	}
 
-    @Override
-    public String toString() {
-        if (this.stackSize == 0) {
-            return "[]";
-        }
+	@Override
+	public String toString() {
+		if (this.stackSize == 0) {
+			return "[]";
+		}
 
-        StringBuilder stringBuilder = new StringBuilder();
-        MyStack myStack = new MyStack<T>();
+		StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = this.stackSize - 1; i >= 0; i--) {
-            elements[i] = getElements()[i];
-            stringBuilder.append(elements[i]).append(", ");
-        }
-        stringBuilder.setLength(stringBuilder.length() - 2);
-        return "[" + stringBuilder.toString() + "] ";
-    }
+		for (int i = this.stackSize - 1; i >= 0; i--) {
+			elements[i] = getElements()[i];
+			stringBuilder.append(elements[i]).append(", ");
+		}
+		stringBuilder.setLength(stringBuilder.length() - 2);
+		return "[" + stringBuilder.toString() + "] ";
+	}
 }
